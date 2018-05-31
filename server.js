a04c0713a7c1921284380c1a8c0d03794d45fd05
stephcmartin <martin.stephaniechow@gmail.com>
2018-05-31 19:54:39 -0400

    Changed to port 5000

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ require('./app/routes/arithmetic.routes.js')(app);
 
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000");
 });
